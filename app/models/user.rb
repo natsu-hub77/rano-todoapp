17531ac6eb267cc56c24a2dc4ba6f7ec9923bd48
らの <>
2025-03-28 20:55:22 +0900

    削除機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :boards, dependent: :destroy
+
+  def has_written?(board)
+    boards.exists?(id: board.id)
+  end
 end
