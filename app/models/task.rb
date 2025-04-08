881f973350bb2f5972749776b1b71094de45c1d0
らの <>
2025-04-09 07:40:30 +0900

    validationの追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,10 @@
 class Task < ApplicationRecord
   validates :title, presence: true
+  validates :title, length: { minimum: 2, maximum: 100 }
   validates :content, presence: true
-
+  validates :content, length: { minimum: 5 }
+  validates :content, uniqueness: true
+  
   has_one_attached :eyecatch
 
   has_many :comments, dependent: :destroy
