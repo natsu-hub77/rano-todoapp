d514c716cace417048f336d66ad1b08eb0c2bdcf
らの <>
2025-04-10 05:59:28 +0900

    データベースのスキーマをモデルに記録

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id         :bigint           not null, primary key
+#  content    :text
+#  deadline   :datetime
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  board_id   :bigint
+#  user_id    :bigint
+#
+# Indexes
+#
+#  index_tasks_on_board_id  (board_id)
+#  index_tasks_on_user_id   (user_id)
+#
 class Task < ApplicationRecord
   validates :title, presence: true
   validates :title, length: { minimum: 2, maximum: 100 }
