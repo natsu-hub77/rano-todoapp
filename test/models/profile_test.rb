ee83097188d2b7696f19b33f9946ed01ed657df3
らの <>
2025-04-12 06:41:24 +0900

    profileモデルの作成

---
+++ test/models/profile_test.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: profiles
+#
+#  id           :bigint           not null, primary key
+#  birthday     :date
+#  department   :string
+#  gender       :integer
+#  introduction :text
+#  name         :string
+#  subscribed   :boolean          default(FALSE)
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  user_id      :bigint           not null
+#
+# Indexes
+#
+#  index_profiles_on_user_id  (user_id)
+#
+require 'test_helper'
+
+class ProfileTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
