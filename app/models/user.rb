dee0984ce8a0573f5873503ab6bf2124560c3025
らの <>
2025-04-14 09:07:51 +0900

    avatar画像のアップロード・表示機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -56,6 +56,14 @@ class User < ApplicationRecord
   #   profile&.gender
   # end
 
+  def avatar_image
+    if profile&.avatar&.attached?
+      profile.avatar
+    else
+      'default-avatar.png'
+    end
+  end 
+
   def prepare_profile
     profile || build_profile
   end
