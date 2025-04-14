f9ddb00fab8bba220f5d7c69d011c9bc70cd9135
らの <>
2025-04-14 20:36:29 +0900

    authenticate_user!の記述を追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_board, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create,]
 
   def new
     task = @board.tasks.find(params[:task_id])
